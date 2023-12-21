b52bfe8f5879eadfd7bb8c75b827e810ed0a36b4
Jay Sabva <202101224@daiict.ac.in>
2023-12-21 22:18:58 +0530

    Static File

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const path = require('path');
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/user', userRoutes);
 
 // ERROR HANDLING
